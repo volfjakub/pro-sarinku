2d46cd2ac709596ce4a2c315625780ed35ecd91c
volfjakub <jakub.volf.cz@gmail.com>
2026-01-31 20:30:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Jsi si jistá?",
+    "Vážně??",
+    "Určitě?",
+    "Prosím, babe...",
+    "Jen o tom popřemýšlej!",
+    "Jestli řekneš ne, budu vážně smutný...",
+    "Budu moc smutný...",
+    "Budu hrozně, hrozně, hrozně smutný...",
+    "Tak dobře, už se nebudu ptát...",
+    "Dělám si srandu, prosím, řekni ano! ❤️"
 ];
 
 let messageIndex = 0;
